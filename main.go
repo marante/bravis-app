df25b7a78c7cfefc94edfc41754c458e3d46073c
marante <bugger@bravis.se>
2018-04-05 19:40:35 +0200

    Changed entire structure, and started working within new branch

--- main.go
+++ main.go
@@ -1,21 +1,16 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
-	"github.com/marante/bravis-app/config"
-	"github.com/marante/bravis-app/dao"
 	"github.com/marante/bravis-app/models"
-	"gopkg.in/mgo.v2/bson"
+	"html/template"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 )
-
-var workorderDao = dao.WorkorderDao{}
-var dbConfig = config.Config{}
 
 // Below code simplifies and makes error handling for handlers more concrete.
 type appError struct {
@@ -34,95 +29,65 @@ func (ah appHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-// CreateWorkorder creates a new workorder
-func CreateWorkorder(w http.ResponseWriter, r *http.Request) *appError {
-	defer r.Body.Close()
-	var order models.Workorder
-	if err := json.NewDecoder(r.Body).Decode(&order); err != nil {
-		fmt.Println(err)
-		return &appError{err, "Error trying to decode json body.", http.StatusBadRequest}
-	}
-	order.ID = bson.NewObjectId()
-	if err := workorderDao.Insert(order); err != nil {
-		fmt.Println(err)
-		return &appError{err, "There was an error when trying to insert into database.", http.StatusBadRequest}
-	}
-	return nil
-}
+var tpl *template.Template
 
-// AllWorkorders return all worksorders from the DB.
-func AllWorkorders(w http.ResponseWriter, r *http.Request) *appError {
-	workorders, err := workorderDao.FindAll()
-	if err != nil {
-		fmt.Println(err)
-		return &appError{err, "Error trying to retrieve records from database", http.StatusBadRequest}
-	}
-	w.Header().Set("Content-type", "application/json")
-	err = json.NewEncoder(w).Encode(workorders)
-	return nil
-}
-
-// FindWorkorder finds a specific workorder by ID
-func FindWorkorder(w http.ResponseWriter, r *http.Request) *appError {
-	params := mux.Vars(r)
-	order, err := workorderDao.FindById(params["id"])
-	if err != nil {
-		fmt.Println(err)
-		return &appError{err, "Error trying to retrieve record from database", http.StatusBadRequest}
-	}
-	w.Header().Set("Content-type", "application/json")
-	err = json.NewEncoder(w).Encode(order)
-	return nil
-}
-
-// UpdateWorkorder updates a specific workorder by ID
-func UpdateWorkorder(w http.ResponseWriter, r *http.Request) *appError {
-	defer r.Body.Close()
-	var order models.Workorder
-	if err := json.NewDecoder(r.Body).Decode(&order); err != nil {
-		fmt.Println(err)
-		return &appError{err, "Error trying to decode json body.", http.StatusBadRequest}
-	}
-	if err := workorderDao.Update(order); err != nil {
-		fmt.Println(err)
-		return &appError{err, "Error trying to update workorder.", http.StatusBadRequest}
-	}
-	return nil
-}
-
-// DeleteWorkorder deletes a speicifc workorder by ID
-func DeleteWorkorder(w http.ResponseWriter, r *http.Request) *appError {
-	defer r.Body.Close()
-	var order models.Workorder
-	if err := json.NewDecoder(r.Body).Decode(&order); err != nil {
-		fmt.Println(err)
-		return &appError{err, "Error trying to decode json body.", http.StatusBadRequest}
-	}
-	if err := workorderDao.Delete(order); err != nil {
-		fmt.Println(err)
-		return &appError{err, "Error trying to delete workorder.", http.StatusBadRequest}
-	}
-	return nil
-}
-
-// Parse the configuration file 'config.toml', and establish a connection to DB
 func init() {
-	dbConfig.Read()
-	workorderDao.Server = dbConfig.Server
-	workorderDao.Database = dbConfig.Database
-	workorderDao.Connect()
+	tpl = template.Must(template.ParseGlob("templates/*.gohtml"))
+}
+
+func Index(w http.ResponseWriter, r *http.Request) *appError {
+	file, err := http.Get("http://localhost:8080/assets/css/styles.css")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Body.Close()
+	data, err := ioutil.ReadAll(file.Body)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Data from the route", string(data))
+	return nil
+}
+
+func Workorder(w http.ResponseWriter, r *http.Request) *appError {
+	workorders := models.Workorder{
+		ObjNr:       "B400",
+		Adress:      "Pilevallsvägen 19",
+		Description: "Vi bygger grejer",
+		Start:       "Påbörjat",
+		Status:      "IDK",
+		Invoice:     "Faktureras inte",
+		Worker: []models.Worker{
+			models.Worker{
+				Name:        "Johan",
+				HoursWorked: 45,
+				Description: "Rev vägg. Målade vägg.",
+			},
+			models.Worker{
+				Name:        "Johan",
+				HoursWorked: 45,
+				Description: "Rev vägg. Målade vägg.",
+			},
+		},
+	}
+	tpl.ExecuteTemplate(w, "index.gohtml", workorders)
+	return nil
 }
 
 func main() {
+	// Get port from env variable
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
 	}
+	// Initializing a gorilla mux router.
 	r := mux.NewRouter()
-	r.Handle("/workorders", appHandler(AllWorkorders)).Methods("GET")
-	r.Handle("/workorders/{id}", appHandler(FindWorkorder)).Methods("GET")
-	r.Handle("/workorders", appHandler(UpdateWorkorder)).Methods("PUT")
-	r.Handle("/workorders", appHandler(CreateWorkorder)).Methods("POST")
-	r.Handle("/workorders", appHandler(DeleteWorkorder)).Methods("DELETE")
+
+	// Creating a fileserver to serve my assets.
+	fs := http.FileServer(http.Dir("assets"))
+	r.PathPrefix("/assets/").Handler(http.StripPrefix("/assets/", fs))
+
+	r.Handle("/", appHandler(Index)).Methods("GET")
+	r.Handle("/workorder", appHandler(Workorder)).Methods("GET")
 	log.Fatal(http.ListenAndServe(":"+port, handlers.LoggingHandler(os.Stdout, r)))
 }
--- models/workorder.go
+++ models/workorder.go
@@ -1,20 +1,17 @@
 package models
 
-import (
-	"gopkg.in/mgo.v2/bson"
-)
-
 type Workorder struct {
-	ID          bson.ObjectId `json:"id" bson:"_id"`
-	ObjNr       string        `json:"objNr" bson:"objNr"`
-	Adress      string        `json:"address,omitempty" bson:"address,omitempty"`
-	Description string        `json:"description,omitempty" bson:"description,omitempty"`
-	Start       string        `json:"start,omitempty" bson:"start,omitempty"`
-	Status      string        `json:"status,omitempty" bson:"status,omitempty"`
-	Invoice     string        `json:"invoice,omitempty" bson:"invoice,omitempty"`
-	Worker      []struct {
-		Name        string  `json:"name,omitempty" bson:"name,omitempty"`
-		HoursWorked float64 `json:"hoursWorked,omitempty" bson:"hoursWorked,omitempty"`
-		Description string  `json:"description,omitempty" bson:"description,omitempty"`
-	} `json:"worker,omitempty" bson:"worker,omitempty"`
+	ObjNr       string `json:"objNr" bson:"objNr"`
+	Adress      string `json:"address,omitempty" bson:"address,omitempty"`
+	Description string `json:"description,omitempty" bson:"description,omitempty"`
+	Start       string `json:"start,omitempty" bson:"start,omitempty"`
+	Status      string `json:"status,omitempty" bson:"status,omitempty"`
+	Invoice     string `json:"invoice,omitempty" bson:"invoice,omitempty"`
+	Worker      []Worker
+}
+
+type Worker struct {
+	Name        string  `json:"name,omitempty" bson:"name,omitempty"`
+	HoursWorked float64 `json:"hoursWorked,omitempty" bson:"hoursWorked,omitempty"`
+	Description string  `json:"description,omitempty" bson:"description,omitempty"`
 }
